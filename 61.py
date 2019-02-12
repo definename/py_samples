462dd0b74ea10e312fcbe3b456fab631d6257b3c
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2019-02-12 23:10:13 +0200

    Numbers and variables.

--- 61.py
+++ 61.py
@@ -1,2 +1,18 @@
 msg = 61
 print(msg)
+
+print(type(123))
+
+print(divmod(9,5))
+
+print(0b10, 0o10, 0x10, sep=",")
+
+b = int("123") + 1
+print(b)
+
+print(True + 1)
+
+googol = 10**100
+print(googol)
+
+print(float("1.1"))
