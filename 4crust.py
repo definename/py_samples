04c74515f02847181a3df6c195a07c74076cad43
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2019-02-19 18:36:46 +0200

    Some control structures were tested.

---
+++ 4crust.py
@@ -0,0 +1,24 @@
+a = 1
+b = 2
+c = 3
+
+e = \
+a + \
+b + \
+c
+print("The sum is:", e)
+
+flag = False
+collection = []
+if collection :
+    print("Hey! There are something in collection")
+else:
+    print("Hey! It's empty")
+
+
+count = 1
+while count <= 5:
+    print(count, end='-')
+    count += 1
+else:
+    print("\nStrange python else was reached")
