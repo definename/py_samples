472a6c59b41de37cbf72b8c5d0b167e857895b33
definename <kolomiytsev.oleg@gmail.com>
2019-03-06 13:42:55 +0200

    Playing with inheritance.

--- 6class/6test.py
+++ 6class/6test.py
@@ -1,9 +1,22 @@
 class Person():
     def __init__(self, name):
-        self.name = name
+        self.name = name + "!"
+
+    def get_name(self):
+        return "Base" + self.name
 
 class ConcretePerson(Person):
-    pass
+    def __init__(self, name):
+        self.name = name + "!!"
 
-give_me_a_concrete_person = ConcretePerson("Person")
-print("Here is concrete person name:", give_me_a_concrete_person.name)
+    def get_name(self):
+        return "Derived" + self.name
+
+    def need_a_push(self):
+        return "Yeah"
+
+person = Person("Name")
+c_person = ConcretePerson("Name")
+print("Here is <base> person name:", person.get_name())
+print("Here is <derived> person name:", c_person.get_name())
+print("Here is derived person need a push:", c_person.need_a_push())
