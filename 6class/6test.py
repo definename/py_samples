2a4936df83262793ffd3c8d5906bea6641c64a55
definename <kolomiytsev.oleg@gmail.com>
2019-03-06 15:18:09 +0200

    super() was tested.

--- 6class/6test.py
+++ 6class/6test.py
@@ -1,22 +1,28 @@
 class Person():
     def __init__(self, name):
+        print("__init__ Person")
         self.name = name + "!"
-
-    def get_name(self):
-        return "Base" + self.name
 
 class ConcretePerson(Person):
     def __init__(self, name):
+        print("__init__ ConcretePerson")
         self.name = name + "!!"
 
     def get_name(self):
         return "Derived" + self.name
 
-    def need_a_push(self):
-        return "Yeah"
-
 person = Person("Name")
 c_person = ConcretePerson("Name")
-print("Here is <base> person name:", person.get_name())
+print("Here is <base> person name:", person.name)
 print("Here is <derived> person name:", c_person.get_name())
-print("Here is derived person need a push:", c_person.need_a_push())
+print("The same as previous call: Here is <derived> person name:", ConcretePerson.get_name(c_person))
+print()
+
+class EmailPerson(Person):
+    def __init__(self, name, email):
+        super().__init__(name)
+        print("__init__ EmailPerson")
+        self.email = email
+
+e_person = EmailPerson("Name", "name@gmail.com")
+print("EmailPerson name:", e_person.name, "email:", e_person.email)
