f93b8ccdbe04cf136fe4ea711324d8515cefa900
definename <kolomiytsev.oleg@gmail.com>
2019-03-20 17:42:56 +0200

    process termination feature has been tested.

--- 10system/py_multiprocess.py
+++ 10system/py_multiprocess.py
@@ -1,10 +1,27 @@
 import multiprocessing
 import os
+import time
 
 def do_this(what):
-    print("Process: {} says: {}".format(os.getpid(), what))
+    while True:
+        print("Process: {} says: {}".format(os.getpid(), what))
+        time.sleep(1)
 
 if __name__ == "__main__":
-    for i in range(4):
-        p = multiprocessing.Process(target=do_this, args=("I'm function {}".format(i),))
+    processList = []
+    for i in range(2):
+        p = multiprocessing.Process(
+            target=do_this,
+            args=("I'm function {}".format(i),),
+            name="NAME{}".format(i))
+        processList.append(p)
+
+    for p in processList:
+        print("\tProcess: {} is being started".format(p.name))
         p.start()
+
+    time.sleep(5)
+
+    for p in processList:
+        print("\tProcess: {} is being terminated".format(p.name))
+        p.terminate()
