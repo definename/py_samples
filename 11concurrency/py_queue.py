5e762b9537542b06ea8574b977b8cf32eb2bd094
definename <kolomiytsev.oleg@gmail.com>
2019-03-21 17:53:07 +0200

    Python process synchronization was tested with JoinableQueue.

---
+++ 11concurrency/py_queue.py
@@ -0,0 +1,31 @@
+import multiprocessing as mp
+
+def washer(dishes, output):
+    for dish in dishes:
+        output.put(dish)
+
+        if (dish == "finish"):
+            print("\tWasher done...")
+        else:
+            print("Wash: {}".format(dish))
+
+def dryer(input):
+    while True:
+        dish = input.get()
+
+        if (dish == "finish"):
+            print("\tDryer done...")
+            input.task_done()
+            break
+
+        print("Dry: {}".format(dish))
+        input.task_done()
+
+if __name__ == "__main__":
+    dish_queue = mp.JoinableQueue()
+    proc = mp.Process(target=dryer, args=(dish_queue,))
+    proc.deamon = True
+    proc.start()
+
+    washer(["salad", "bread", "entee", "dessert", "finish"], dish_queue)
+    dish_queue.join()
