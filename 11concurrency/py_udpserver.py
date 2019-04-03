5912f84397f114a01894a6ee9d199fd460698a76
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-04-03 17:49:50 +0300

    Added functionality to stop udp server.

--- 11concurrency/py_udpserver.py
+++ 11concurrency/py_udpserver.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import socket
 import logging
+import threading
 
 streamHandler = logging.StreamHandler()
 streamHandler.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)s - %(message)s'))
@@ -11,15 +12,44 @@ logger.addHandler(streamHandler)
 max_size = 4096
 addr = ("localhost", 6789)
 
-try:
+def CloseSocket(server):
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
+
+def ServerHandler(server):
+    try:
+        server.bind(addr)
+
+        data, client_addr = server.recvfrom(max_size)
+        logger.debug("At {} client: {} said: {}".format(datetime.now(), client_addr, data.decode("utf-8")))
+
+        server.sendto(b"Are you talking to me?", client_addr)
+        CloseSocket(server)
+    except Exception as e:
+        logger.error("UDP server handler error: {}".format(e))
+
+desc = """Usage:
+    'q' - exit;
+    '?' - get this help.
+"""
+
+def main():
     logger.debug("UDP server is being started at: {}".format(addr))
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind(addr)
 
-    data, client_addr = server.recvfrom(max_size)
-    logger.debug("At {} client: {} said: {}".format(datetime.now(), client_addr, data.decode("utf-8")))
+    thread = threading.Thread(target=ServerHandler, args=(server,))
+    thread.start()
 
-    server.sendto(b"Are you talking to me?", client_addr)
-    server.close()
-except Exception as e:
-    logger.error("UDP client error: {}".format(e))
+    while True:
+        val = input(desc)
+        if val == "q":
+            CloseSocket(server)
+            break
+        elif val == "?":
+            logging.debug(desc)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.error("UDP server error: {}".format(e))
