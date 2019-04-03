01d807aff5fa582381da7a55ada00e96e41b83d1
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-04-03 13:08:10 +0300

    Print json pretty.

--- 08save-data/py_json.py
+++ 08save-data/py_json.py
@@ -1,6 +1,6 @@
 menu1 = {
     'breakfast': { 'hours': '7-11' },
-    'lunch': { 'hours': '11-3' },
+    'launch': { 'hours': '11-3' },
     'dinner': { 'hours': '3-10' }
     }
 
@@ -8,8 +8,10 @@ import json
 
 # serialize dictionary into json string.
 jsonMenu = json.dumps(menu1)
-print("{}: {}".format(type(jsonMenu), jsonMenu),)
+print("Serialize:\n{}: {}".format(type(jsonMenu), jsonMenu),)
 
 # deserialize json string into dictionary.
 menu2 = json.loads(jsonMenu)
-print("{}: {}".format(type(menu2), menu2))
+print("Deserialize:\n{}: {}".format(type(menu2), menu2))
+
+print("Pretty:\n{}".format(json.dumps(menu1, indent=2)))
