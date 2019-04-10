7c7586fd3d49ed27a48230953638e0dad437e0d7
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-04-10 17:24:46 +0300

    setUp/tearDown function were added in order to be called at start/end test.

--- py_test/test_unittests.py
+++ py_test/test_unittests.py
@@ -3,6 +3,12 @@ import unittest
 
 
 class Test_TestIncrementDecrement(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
     def test_increment(self):
         self.assertEqual(inc_dec.increment(3), 4)
 
