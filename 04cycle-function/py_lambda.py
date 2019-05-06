49e286be9eb2f52c3a149a40ded60cc249982ea1
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-05-06 11:49:42 +0300

    lambda sample formatting.

--- 04cycle-function/py_lambda.py
+++ 04cycle-function/py_lambda.py
@@ -1,9 +1,11 @@
 # lambda
 words = ["word1", "word2", "word3"]
 
+
 def edit_story(to_edit, func):
     for word in to_edit:
         print(func(word))
+
 
 edit_story(words, lambda word: word.capitalize() + "!")
 
@@ -16,5 +18,5 @@ ls1 = [(1, 3), (3, 1), (2, 2)]
 ls1.sort()
 print("x[0] by default {}".format(ls1))
 
-ls1.sort(key = lambda x: x[1])
+ls1.sort(key=lambda x: x[1])
 print("x[1]: {}".format(ls1))
