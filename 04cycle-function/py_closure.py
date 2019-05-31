4a1477bfb4ae4f701d1465c1f5e91e9fa3ab275f
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-05-31 13:13:59 +0300

    Py closure with predef params.

--- 04cycle-function/py_closure.py
+++ 04cycle-function/py_closure.py
@@ -37,23 +37,24 @@ class EngineAsync():
     def __init__(self):
         self.__sn = None
 
-    def set_serial(self, sn):
+    def set_serial(self, sn, done_id):
         self.__sn = sn
-        log.debug("set_serial: {}".format(self.__sn))
+        log.debug("done_id: {} set_serial: {}".format(done_id, self.__sn))
     
-    def get_serial(self):
-        log.debug("get_serial: {}".format(self.__sn))
+    def get_serial(self, done_id):
+        log.debug("done_id: {} get_serial: {}".format(done_id, self.__sn))
         return self.__sn
 
 class Engine(EngineAsync):
     def __init__(self):
         super().__init__()
         self.__name = "name"
+        self.__done_id = 1
 
     def __quard(self, func):
         def inner(*args, **kwargs):
             log.debug("Decorated")
-            return func(*args, **kwargs)
+            return func(*args, self.__done_id, **kwargs)
         return inner
 
     def set_serial(self, sn):
