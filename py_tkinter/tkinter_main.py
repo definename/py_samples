593b1204c290b2e8bac74b219cf504508b2f0529
definename <kolomiytsev.oleg@gmail.com>
2019-06-04 22:22:23 +0300

    Initial tkinter sample.

---
+++ py_tkinter/tkinter_main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    l = tk.Label(root, width=20)
+    l["text"] = "Lable"
+    l.pack()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
