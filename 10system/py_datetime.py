f5e1d9474e58151d1cd43b9f4270e014f72a9230
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-06-18 15:10:57 +0300

    Parse datetime string.

--- 10system/py_datetime.py
+++ 10system/py_datetime.py
@@ -1,32 +1,37 @@
 import calendar
+import logging
 
-print("is leap: {}".format(calendar.isleap(1987)))
-print("is leap: {}".format(calendar.isleap(2020)))
+logging.basicConfig(level=logging.DEBUG,
+                    format="{asctime} {name} {levelname} - {message}", style="{")
+log = logging.getLogger()
+
+log.debug("is leap: {}".format(calendar.isleap(1987)))
+log.debug("is leap: {}".format(calendar.isleap(2020)))
 
 # date
 from datetime import date
 
 someDate = date.today()
-print("Some date: {}".format(someDate.isoformat()))
+log.debug("Some date: {}".format(someDate.isoformat()))
 
 # timedelta
 from datetime import timedelta
 
 oneDay = timedelta(days=1)
 tomorrow = someDate + oneDay
-print("+1 day: {}".format(tomorrow.isoformat()))
+log.debug("+1 day: {}".format(tomorrow.isoformat()))
 
 yesterday = someDate - oneDay
-print("-1 day: {}".format(yesterday.isoformat()))
+log.debug("-1 day: {}".format(yesterday.isoformat()))
 
 # time
 from datetime import time
 someTime = time(23, 12, 31)
-print("{}".format(someTime.isoformat()))
+log.debug("{}".format(someTime.isoformat()))
 
 from datetime import datetime
 some = datetime.combine(someDate, someTime)
-print("Some datetime {}".format(some.isoformat()))
+log.debug("Some datetime {}".format(some.isoformat()))
 
 # write to file in isoformat
 iofile = open("dt.dat", "w+")
@@ -43,15 +48,24 @@ while True:
         break
     today_string += line
 
-print("\tWritten: {}".format(today_string))
+log.debug("\tWritten: {}".format(today_string))
 
 # parse isoformat
 dt = datetime.fromisoformat(today_string)
-print("\tParsed: {}".format(dt.isoformat()))
+log.debug("\tParsed: {}".format(dt.isoformat()))
 
 # bday week day
 bday = date(1987, 12, 24)
-print("Week day: {}".format(bday.weekday()))
+log.debug("Week day: {}".format(bday.weekday()))
 
 # 10 000 day from bday
-print("10 000 day was born: {}".format(bday + timedelta(days=10000)))
+log.debug("10 000 day was born: {}".format(bday + timedelta(days=10000)))
+
+log.debug("=== parse datetime string")
+
+yymd_raw = "20190618"
+try:
+    yymd_date = datetime.strptime(yymd_raw, r"%Y%m%d")
+    log.info("yy({}) m({}) d({})".format(yymd_date.year, yymd_date.month, yymd_date.day))
+except ValueError as e:
+    log.error("Failed to parse date: {}".format(e))
