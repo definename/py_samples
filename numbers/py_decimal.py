6f97f7db266ffc46772e157b8aabfc06ae279fbd
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-06-21 18:23:47 +0300

    Decimal context tricks.

--- numbers/py_decimal.py
+++ numbers/py_decimal.py
@@ -1,11 +1,19 @@
 #! /usr/bin/python3
 
 import logging
-from decimal import Decimal
+import decimal
 
 logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} {name} {levelname} - {message}", style="{")
 log = logging.getLogger()
 
 log.debug(0.1 + 0.1 + 0.1 - 0.3)
-log.debug(Decimal("0.1") + Decimal("0.1") + Decimal("0.1") - Decimal("0.3"))
+
+d01 = decimal.Decimal("0.1")
+d03 = decimal.Decimal("0.3")
+log.debug(d01 + d01 + d01 - d03)
+
+decimal.getcontext().prec = 3
+d10 =  decimal.Decimal(10)
+d3 =  decimal.Decimal(3)
+log.debug(d10 / d3)
