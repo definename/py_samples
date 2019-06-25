13486a731ca57c589f45a0721b706df0b6bdefbe
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-06-25 20:10:37 +0300

    Added functionality to xor.

--- py_bytes.py
+++ py_bytes.py
@@ -7,21 +7,22 @@ log = logging.getLogger()
 
 test = "aa550018000000dcffff0006ffffffff0003ffff000a0007ffff0000f5140d0a"
 log.debug(f"test {test}")
-log.debug(f"test length: {len(test)}")
 
 log.debug("{:=^100s}".format("!!!"))
 
-insert_block = bytes.fromhex("ffff")
-block_size = len(insert_block)
+raw_block = bytes.fromhex("f0f0")
+log.debug(f"raw_block {raw_block.hex()}")
+block_size = len(raw_block)
 log.debug(f"block size: {block_size}")
 
 raw_test = bytes.fromhex(test)
+log.debug(f"raw_test length: {len(raw_test)}")
 
 insert_front = raw_test[0:10]
 insert_back = raw_test[10:]
 log.debug(f"insert insert_front {insert_front}")
 log.debug(f"insert insert_back {insert_back}")
-raw_glued = b"".join([insert_front, insert_block, insert_back])
+raw_glued = b"".join([insert_front, raw_block, insert_back])
 log.debug(f"raw_glued {raw_glued}")
 log.debug(f"raw_glued hex {raw_glued.hex()}")
 
@@ -35,3 +36,20 @@ log.debug(f"del_front {del_front.hex()}")
 log.debug(f"del_back {del_back.hex()}")
 raw_deleted = b"".join([del_front, del_back])
 log.debug(f"raw_deleted {raw_deleted.hex()}")
+
+
+log.debug("{:=^100s}".format("!!!"))
+pos = random.randrange(start=0, stop=len(raw_test) - block_size + 1)
+log.debug(f"xor pos: {pos}")
+raw_chunk = raw_test[pos:pos + block_size]
+log.debug(f"raw_chunk {raw_chunk.hex()}")
+
+chunk_xored = b""
+for each, key in zip(raw_chunk, raw_block):
+    res = each ^ key
+    res_hex = format(res, "02x")
+    log.debug(res_hex)
+    chunk_xored = b"".join([chunk_xored, bytes.fromhex(res_hex)])
+
+log.debug(f"chunk_xored: {chunk_xored.hex()}")
+log.debug(b"".join([raw_test[0:pos], chunk_xored, raw_test[pos+block_size:]]).hex())
