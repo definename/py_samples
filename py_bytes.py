163526c8e34c9007c6651f9a8615e2455b893d74
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-06-25 16:47:41 +0300

    Insert into byte string.

--- py_bytes.py
+++ py_bytes.py
@@ -4,6 +4,15 @@ logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} {name} {levelname} - {message}", style="{")
 log = logging.getLogger()
 
-b = b'0003'
-log.debug(b)
-log.debug(bytes.fromhex("30303033"))
+test = "aa550018000000dcffff0006ffffffff0003ffff000a0007ffff0000f5140d0a"
+insert_block = bytes.fromhex("ffff")
+raw_test = bytes.fromhex(test)
+log.debug(f"test {raw_test}")
+raw_front = raw_test[0:10]
+raw_back = raw_test[10:]
+log.debug(f"front {raw_front}")
+log.debug(f"back {raw_back}")
+raw_glued = b"".join([raw_front, insert_block, raw_back])
+log.debug(f"glued {raw_glued}")
+log.debug(f"raw_glued hex {raw_glued.hex()}")
+
