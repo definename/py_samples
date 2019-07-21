d6d3e7694a61036be412c19eec3f5b4130cd9f76
definename <kolomiytsev.oleg@gmail.com>
2019-07-21 22:29:32 +0300

    nonlocal namespace.

--- py_nonlocal.py
+++ py_nonlocal.py
@@ -4,9 +4,17 @@ logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} {name} {levelname} - {message}", style="{")
 log = logging.getLogger()
 
+def foo0():
+    def foo1():
+        def foo2():
+            log.debug(f">>>>>>>>>>>>: {x}")
+        foo2()
+    foo1()
+
 x = 99
 
 def main():
+    foo0()
     x = 100
     def foo():
         log.debug(f"x: {x}")
