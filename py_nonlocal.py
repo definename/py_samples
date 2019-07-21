902e8a62ef8881019f22bfbd4306e564ab6fe69a
definename <kolomiytsev.oleg@gmail.com>
2019-07-21 23:07:01 +0300

    Counter with nonlocal

--- py_nonlocal.py
+++ py_nonlocal.py
@@ -4,21 +4,41 @@ logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} {name} {levelname} - {message}", style="{")
 log = logging.getLogger()
 
+x = 99
+
+def globan_or_nonlocal():
+    x = 100
+    def foo():
+        log.debug(f"!!!: {x}")
+    foo()
+
+def counter(start):
+    state = start
+    def nested(fruit):
+        nonlocal state
+        log.debug(f"{fruit}: {state}")
+        state += 1
+    return nested
+
 def foo0():
     def foo1():
         def foo2():
-            log.debug(f">>>>>>>>>>>>: {x}")
+            log.debug(f">>>: {x}")
         foo2()
     foo1()
 
-x = 99
-
 def main():
+    # Global on non local
+    globan_or_nonlocal()
+
+    # Nestaed function and nonlocal
     foo0()
-    x = 100
-    def foo():
-        log.debug(f"x: {x}")
-    foo()
+
+    # Nonlocal counter
+    apple = counter(0)
+    apple("apple")
+    apple("apple")
+    apple("apple")
 
 if __name__ == "__main__":
     main()
