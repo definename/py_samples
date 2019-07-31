fd21136b75a96a6b84383cd774d8ce98401b3c01
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-07-31 17:00:49 +0300

    Rand uuid

--- py_uuid.py
+++ py_uuid.py
@@ -6,7 +6,12 @@ logging.basicConfig(level=logging.DEBUG,
 log = logging.getLogger()
 
 # uuid name generator
+log.debug("Namespace based uuid:")
 NAMESPACE_HANDLER = uuid.UUID("{18a42823-3a4d-4aaf-84b6-816e2b74c788}")
 log.debug("{}".format(uuid.uuid3(NAMESPACE_HANDLER, "handler1")))
 log.debug("{}".format(uuid.uuid3(NAMESPACE_HANDLER, "handler2")))
 log.debug("{}".format(uuid.uuid3(NAMESPACE_HANDLER, "handler3")))
+
+log.debug("Random uuid:")
+rand_uuid = str(uuid.uuid4().int) + "serial1"
+log.debug(rand_uuid)
