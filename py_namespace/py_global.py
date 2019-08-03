2c1f5bca64a6d235de78efc3208864730cf4c395
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-08-04 00:48:46 +0300

    Py global naming fixes.

--- py_namespace/py_global.py
+++ py_namespace/py_global.py
@@ -6,12 +6,12 @@ log = logging.getLogger()
 
 x = 1
 y = 1
-def foo():
+def create_global_in_func():
     global z
     z = x + y
     return z
 
-def foo1():
+def change_global_using_build_in():
     import py_global as my
     glob = globals()
     glob["x"] += 1
@@ -24,12 +24,12 @@ def local_and_global_access():
 
 def main():
     # Create global var in function.
-    log.debug(f"create global z: {foo()}")
+    log.debug(f"create global z: {create_global_in_func()}")
     log.debug(f"global z: {z}")
 
     # Change global var with build-in function.
     log.debug(f"x: {x}")
-    foo1()
+    change_global_using_build_in()
     log.debug(f"x: {x}")
 
     # Access global and local variable with the same name
