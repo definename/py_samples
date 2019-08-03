20c01ca19cea3290d8a489552943b10c57184ee4
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-08-03 22:18:43 +0300

    Extended dict generator.

--- py_generator/dict_generator.py
+++ py_generator/dict_generator.py
@@ -11,6 +11,9 @@ def main():
     d = { letter: letter*2 for letter in word}
     log.debug(d)
 
+    dd = {x : y for x in [1, 2, 3] for y in [4, 5, 6]}
+    log.debug(f"Dict doesn't allow duplicates: {dd}")
+
 if __name__ == "__main__":
     main()
 
