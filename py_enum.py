7469f1879fd2ebdf348b64bb5b194686cba6b235
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-08-14 13:27:28 +0300

    Enum with hex number.

--- py_enum.py
+++ py_enum.py
@@ -9,13 +9,12 @@ logging.basicConfig(level=logging.DEBUG,
 log = logging.getLogger()
 
 class Cmd_t(enum.Enum):
-    CMD_ONE2 = 1
-    CMD_ONE1 = 1
-    CMD_TWO = 2
+    CMD_ONE = 0x1
+    CMD_TWO = 0x2
 
 if __name__ == "__main__":
     log.debug(f"{Cmd_t(1)}")
     try:
-        Cmd_t(3)
+        log.debug(f"{Cmd_t(3)}")
     except ValueError as e:
         log.error(f"Error occured: {e}")
