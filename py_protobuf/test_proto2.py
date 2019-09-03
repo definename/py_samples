51e670dbf55684e3908ace846689d92f38a6b5ef
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-09-03 12:43:53 +0300

    Py protobuf2 tested.

---
+++ py_protobuf/test_proto2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import logging
+from proto import test_proto2_pb2 as tp2
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="{asctime} {name} {levelname} - {message}", style="{")
+log = logging.getLogger()
+
+if __name__ == "__main__":
+    try:
+        log.debug("Serialize...")
+        test = tp2.Test()
+
+        item_str = test.items.add()
+        item_str.key = tp2.Item.DATA1
+        item_str.str = "stringpayload"
+
+        item_int = test.items.add()
+        item_int.key = tp2.Item.DATA2
+        item_int.i32 = 123
+
+        log.debug(f"Test:{test}")
+        raw_data = test.SerializeToString()
+        log.debug(f"Serialized test:{raw_data.hex()}")
+
+        test.Clear()
+        test.ParseFromString(raw_data)
+
+        for item in test.items:
+            log.debug(f"key:{item.key}")
+            if item.key == tp2.Item.DATA1:
+                log.debug(f"value:{item.str}")
+            elif item.key == tp2.Item.DATA2:
+                log.debug(f"value:{item.i32}")
+            else:
+                log.debug("Unknown value...")
+    except Exception as e:
+        log.exception(f"Error occurred:{e}")
