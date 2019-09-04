6cc9fb1da532257590fc88ba5f76c615131613f6
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-09-04 22:06:43 +0300

    Useless testpackets were removed from the repo.

--- py_packet/packet/sub_packet/mod.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG,
-                    format="{asctime} {name} {levelname} - {message}", style="{")
-log = logging.getLogger()
-
-def do():
-    log.debug("do!!!")
--- py_packet/packet/weekly.py
+++
@@ -1,5 +0,0 @@
-def forecast():
-    '''
-    Fake weekly forecast
-    '''
-    return ['rain', 'snow', 'sleet', 'fog', 'sun', 'who knows']
