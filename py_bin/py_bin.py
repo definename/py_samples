14dfa6bcc9cd3fe5126292b42fd37648b8f8c411
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-09-17 15:41:27 +0300

    Split 8bit number.

---
+++ py_bin/py_bin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.6
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("test")
+
+def main():
+    ioseq = int("11101111", 2)
+    oseq = ioseq & int("f", 16)
+    iseq = ioseq & int("f0", 16)
+    log.debug("{} {:08b}".format(oseq, oseq))
+    log.debug("{} {:08b}".format(iseq, iseq))
+
+if __name__ == "__main__":
+    main()
