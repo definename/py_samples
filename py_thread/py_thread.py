0edbe3e311c1a6dd985b33402ba4b5193ea0cc45
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-10-23 00:21:59 +0300

    Washer/drier threading fixes.

--- py_thread/py_thread.py
+++ py_thread/py_thread.py
@@ -7,42 +7,59 @@ logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} {name} {levelname} - {message}", style="{")
 log = logging.getLogger()
 
+is_running = True
+dish_queue = queue.Queue()
 
 def washer(dishes, dish_queue):
     for dish in dishes:
-        time.sleep(1)
+        time.sleep(0.5)
         dish_queue.put(dish)
 
-        if dish == "exit":
+        if dish is None:
             log.debug("Washer done...")
         else:
-            log.debug("Wash: {}".format(dish))
+            log.debug(f"Wash:{dish}")
 
 
-def dryer(dish_queue):
-    while True:
-        time.sleep(2)
-        dish = dish_queue.get()
-
-        if dish == "exit":
-            log.debug("Dryer done...")
-            dish_queue.task_done()
-            break
-
-        log.debug("Dry: {}".format(dish))
-        dish_queue.task_done()
+def dryer():
+    try:
+        while is_running:
+            try:
+                dish = dish_queue.get(timeout=1)
+                dish_queue.task_done()
+                if dish is None:
+                    break
+                log.debug(f"Dry dish:{dish} is running:{is_running}")
+            except queue.Empty:
+                log.warning("Dryer queue is empty...")
+        log.debug("Dryer done...")
+    except Exception as e:
+        log.error(f"Dryer error:{e}")
 
 
 def main():
-    dish_queue = queue.Queue()
-    thread = threading.Thread(target=dryer, args=(dish_queue,))
-    thread.start()
+    try:
 
-    washer(["salad", "bread", "entee", "desert", "exit"], dish_queue)
-    dish_queue.join()
+        thread = threading.Thread(target=dryer)
+        thread.start()
 
-    log.debug("Done...")
+        washer(["salad", "bread", "entee", "desert", None], dish_queue)
 
+        dish_queue.join()
+        log.debug("Queue joined...")
+        thread.join()
+        log.debug("Thread joined...")
+    except KeyboardInterrupt:
+        log.debug("Keyboard interrupted...")
+        global is_running
+        is_running = False
+
+        dish_queue.join()
+        log.debug("Queue joined...")
+        thread.join()
+        log.debug("Thread joined...")
+    except Exception as e:
+        log.error(f"Error occurred:{e}")
 
 if __name__ == "__main__":
     main()
