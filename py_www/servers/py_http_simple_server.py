300b0ff6915c62f783d9bca25f08beb5606e7e1e
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-11-20 11:45:35 +0200

    Use http server log_message function instead of log.debug directly.

--- py_www/servers/py_http_simple_server.py
+++ py_www/servers/py_http_simple_server.py
@@ -10,22 +10,24 @@ log = logging.getLogger(__name__)
 class MyHttpHandler(http.server.BaseHTTPRequestHandler):
     
     def do_GET(self):
-        # log.debug(f"CLIENTADDR:\n{self.client_address}")
-        # log.debug(f"REQUESTLINE:\n{self.requestline}")
-        log.debug(f"HEADERS:\n{type(self.headers)}{self.headers}")
-        # log.debug(f"RESPONSES:\n{type(self.responses)}{self.responses}")
+        # self.log_message(f"CLIENTADDR:\n{self.client_address}")
+        # self.log_message(f"REQUESTLINE:\n{self.requestline}")
+        self.log_message(f"HEADERS:\n{type(self.headers)}{self.headers}")
+        # self.log_message(f"RESPONSES:\n{type(self.responses)}{self.responses}")
         self.send_response(http.server.HTTPStatus.OK)
         self.send_header("my","header")
         self.end_headers()
 
+    def log_message(self, format, *args):
+        log.debug(format, *args)
+
 
 def main():
     try:
-        bind_addr = ("localhost", 8000)
-        # server_handler = http.server.SimpleHTTPRequestHandler
+        server_addr = ("", 8000)
         server_handler = MyHttpHandler
-        httpd = http.server.HTTPServer(bind_addr, server_handler)
-        log.debug(f"Socket has been bound on:{bind_addr}")
+        httpd = http.server.HTTPServer(server_addr, server_handler)
+        log.debug(f"Socket has been bound on:{server_addr}")
 
         # keyfile="server_key.key"
         # certfile="server_crt.crt"
