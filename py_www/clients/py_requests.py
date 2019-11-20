b9787b40ed98bc24c16bc9517360b5a3f0f27504
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-11-20 16:05:55 +0200

    Http client fixes.

--- py_www/clients/py_requests.py
+++ py_www/clients/py_requests.py
@@ -1,8 +1,17 @@
+#!/usr/bin/python3
+
 import requests
-url = "http://google.com"
+import logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="{asctime} {name} {levelname} - {message}", style="{")
+
+# url = "http://google.com"
+url = "http://172.30.227.84:8000/scritps/Config_0"
 resp = requests.get(url)
 
-print("Status code: {}".format(resp), end="\n\n")
+log.debug(f"Status code:{resp.status_code}")
+log.debug(f"Reason:{resp.reason}")
 
 for key, value in resp.headers.items():
-    print(key, value)
+    log.info(f"Header:{key}:{value}")
--- py_www/clients/py_urllib.py
+++ py_www/clients/py_urllib.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
 import urllib.request as ur
-url = "http://google.com"
+# url = "http://google.com"
+# url = "http://172.30.227.84:8000"
+url = "http://172.30.227.84:8000/scripts/Config_0"
 conn = ur.urlopen(url)
 
 print("Status code: {}".format(conn.status), end="\n\n")
