9f870f6dce76f696473bf29cea2c4a47692d8be5
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-11-21 16:33:50 +0200

    play with py urllib.parse.

---
+++ py_url/py_urlparse.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import logging
+import urllib
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+def test_string_split(test_path):
+    log.debug("................................")
+    log.debug(test_path)
+
+    path_res = test_path.split("#", 1)
+    log.debug(path_res)
+    test_path = path_res[0]
+    log.debug(test_path)
+
+    path_res = test_path.split("?", 1)
+    log.debug(path_res)
+    test_path = path_res[0]
+    log.debug(test_path)
+
+def test_urlparse(test_path):
+    log.debug("................................")
+    res = urlparse(test_path)
+    log.debug(res)
+    res = urllib.parse.parse_qs(res.query)
+    log.debug(res)
+    
+
+def test_urlsplit(test_path):
+    log.debug("................................")
+    res= urlsplit(test_path)
+    log.debug(res)
+
+
+def main():
+    test_path = "/scripts/run_something.sh?chunk=2048&sleep=2&disconnect=2#fragment"
+    test_string_split(test_path)
+    test_urlparse(test_path)
+    test_urlsplit(test_path)
+
+
+
+if __name__ == "__main__":
+    main()
