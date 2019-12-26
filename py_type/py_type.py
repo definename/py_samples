f3f1f8ed80e0f5eb3754baa35a3005bd6638dc71
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-26 11:26:17 +0200

    Minor fixes.

--- py_type/py_type.py
+++ py_type/py_type.py
@@ -7,10 +7,13 @@ logging.basicConfig(level=logging.DEBUG,
 log = logging.getLogger()
 
 def main():
-    param = 1.1
+    param = True
     log.debug(f"It works: {type(param)}")
     if (type(param) is float):
         log.debug("Yes it is float")
+    elif (type(param) is bool):
+        log.debug("Yes it is bool")
+
 
 if __name__ == "__main__":
     main()
