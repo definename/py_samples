2169e7221489ebffcfed651aaaf1e277f51b5102
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-01-01 21:43:19 +0200

    Search for something in file.

---
+++ py_file/py_search_in_file.py
@@ -0,0 +1,31 @@
+import logging
+import argparse
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", dest="file", help="Source file path", required=True)
+    parser.add_argument("-s", dest="search", help="Search statement", required=True)
+    args = parser.parse_args()
+
+    log.debug("Search:'{}' in '{}'".format(args.search, args.file))
+
+    found_list = []
+    with open(file=args.file, mode="rt") as stream:
+        for line in stream:
+            if args.search in line:
+                found_list.append(line)
+
+    if len(found_list) == 0:
+        log.debug("Nothing found...")
+    else:
+        for found_in in found_list:
+            log.debug(found_in)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        log.exception("{}".format(e))
